f1e12519a8ad57c74c0edbd328fb38fa1b0c966d
panqueva764 <panqueva764@gmail.com>
2024-02-24 00:22:29 -0500

    Implementa Api's Projects

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+/*
+|--------------------------------------------------------------------------
+| Project Routes
+|--------------------------------------------------------------------------
+|
+| Here are the routes for managing projects in the API. These routes allow
+| you to perform CRUD (Create, Read, Update, Delete) operations on projects.
+| All routes are prefixed with '/projects'.
+|
+*/
+Route::prefix('projects')->group(function () {
+    Route::get('/', [ProjectController::class, 'index']);
+    Route::post('/', [ProjectController::class, 'store']);
+    Route::get('/{id}', [ProjectController::class, 'show']);
+    Route::put('/{id}', [ProjectController::class, 'update']);
+    Route::delete('/{id}', [ProjectController::class, 'destroy']);
 });
