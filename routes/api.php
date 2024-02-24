2286c1910a405bd61664015f568cde3de893df9f
panqueva764 <panqueva764@gmail.com>
2024-02-23 22:18:43 -0500

    Implementa Api's Certificates

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CertificateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+/*
+|--------------------------------------------------------------------------
+| Certificates Routes
+|--------------------------------------------------------------------------
+|
+| Here are the routes for managing Certificates in the API. These routes allow
+| you to perform CRUD (Create, Read, Update, Delete) operations on Certificates.
+| All routes are prefixed with '/Certificates'.
+|
+*/
+Route::prefix('certificates')->group(function () {
+    Route::get('/', [CertificateController::class, 'index']);
+    Route::post('/', [CertificateController::class, 'store']);
+    Route::get('/{id}', [CertificateController::class, 'show']);
+    Route::put('/{id}', [CertificateController::class, 'update']);
+    Route::delete('/{id}', [CertificateController::class, 'destroy']);
 });
